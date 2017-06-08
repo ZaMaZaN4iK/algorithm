da4f454f14e59e6a21e77fb78bc1c6ce8bc11e7d
Evgenii <eabesea@rambler.ru>
2017-06-09 00:55:34 +0300

    Refactoring

--- include/boost/algorithm/fuzzy_search/fuzzy_search.hpp
+++ include/boost/algorithm/fuzzy_search/fuzzy_search.hpp
@@ -17,152 +17,152 @@
 #include <vector>
 
 /**
- *
+ * Helper function for preprocessing of data for search with Hamming difference
  * @tparam ForwardIt1
  * @tparam SearchedType
  * @param first
  * @param last
- * @param k
- * @param q
- * @param i
- * @param s
- * @param S1
- * @param M
- * @param Ds
- * @param D
+ * @param k number of allowed mistakes
+ * @param q q-gram length
+ * @param i current recursion depth
+ * @param s current qgram prefix
+ * @param S1 alphabet
+ * @param M array of differences between qgram and needle, where deletion in the beginning are free
+ * @param Ds array of the lengthes of jumps for qgrams
+ * @param D array to calculate distance between qgram and needle
  */
 template <class ForwardIt1, class SearchedType>
-void preprocess_helper_mismatch(ForwardIt1 first,
-                                ForwardIt1 last,
-                                size_t m,
-                                size_t k,
-                                size_t q,
-                                size_t i,
-                                std::vector<SearchedType> &s,
-                                std::unordered_set<SearchedType> &S1,
-                                std::map<std::vector<SearchedType>, size_t> &M,
-                                std::map<std::vector<SearchedType>, size_t> &Ds,
-                                std::vector<std::vector<size_t>> &D) {
+void preprocess_helper_hamming(ForwardIt1 first,
+                               ForwardIt1 last,
+                               size_t m,
+                               size_t k,
+                               size_t q,
+                               size_t i,
+                               std::vector<SearchedType> &s,
+                               std::unordered_set<SearchedType> &S1,
+                               std::map<std::vector<SearchedType>, size_t> &M,
+                               std::map<std::vector<SearchedType>, size_t> &Ds,
+                               std::vector<size_t> &D) {
   if (i == q + 1) {
-    M[s]  = D[q][m];
-    Ds[s] = std::find_if(D[q].rbegin() + 1, D[q].rend(), [k](size_t j) { return j <= k; }) - D[q].rbegin();
+    M[s]  = D.back();
+    Ds[s] = std::find_if(D.rbegin() + 1, D.rbegin() + m + 1, [k](size_t j) { return j <= k; }) - D.rbegin();
   } else {
     s.push_back(*S1.begin());
     for (auto &c : S1) {
+      auto curr = first;
       for (int j = 1; j <= m; ++j) {
-        D[i][j] = D[i - 1][j - 1] + ((c == *std::next(first, j - 1)) ? 0 : 1); //TODO: 1D
+        D[i * (m + 1) + j] = D[(i - 1) * (m + 1) + j - 1] + ((c == *(curr++)) ? 0 : 1);
       }
-      // TODO: For maximum performance it is crucial how the value of a q-gram is computed during searching.
       s.back() = c;
-      preprocess_helper_mismatch(first, last, m, k, q, i + 1, s, S1, M, Ds, D);
+      preprocess_helper_hamming(first, last, m, k, q, i + 1, s, S1, M, Ds, D);
     }
     s.pop_back();
   }
 }
 
 /**
- *
+ * Helper function for preprocessing of data for search with Levenshtein difference
  * @tparam ForwardIt1
  * @tparam SearchedType
  * @param first
  * @param last
- * @param m
- * @param k
- * @param q
- * @param i
- * @param s
- * @param S1
- * @param M
- * @param Ds
- * @param D
+ * @param k number of allowed mistakes
+ * @param q q-gram length
+ * @param i current recursion depth
+ * @param s current qgram prefix
+ * @param S1 alphabet
+ * @param M array of differences between qgram and needle, where deletion in the beginning are free
+ * @param Ds array of the lengthes of jumps for qgrams
+ * @param D array to calculate distance between qgram and needle
  */
 template <class ForwardIt1, class SearchedType>
-void preprocess_helper_difference(ForwardIt1 first,
-                                  ForwardIt1 last,
-                                  size_t m,
-                                  size_t k,
-                                  size_t q,
-                                  size_t i,
-                                  std::vector<SearchedType> &s,
-                                  std::unordered_set<SearchedType> &S1,
-                                  std::map<std::vector<SearchedType>, size_t> &M,
-                                  std::map<std::vector<SearchedType>, size_t> &Ds,
-                                  std::vector<std::vector<size_t>> &D) {
+void preprocess_helper_levenshtein(ForwardIt1 first,
+                                   ForwardIt1 last,
+                                   size_t m,
+                                   size_t k,
+                                   size_t q,
+                                   size_t i,
+                                   std::vector<SearchedType> &s,
+                                   std::unordered_set<SearchedType> &S1,
+                                   std::map<std::vector<SearchedType>, size_t> &M,
+                                   std::map<std::vector<SearchedType>, size_t> &Ds,
+                                   std::vector<size_t> &D) {
   if (i == q + 1) {
-    M[s]  = D[q][m];
-    Ds[s] = std::find_if(D[q].rbegin() + 1, D[q].rend(), [k](size_t j) { return j <= k; }) - D[q].rbegin();
+    M[s]  = D.back();
+    Ds[s] = std::find_if(D.rbegin() + 1, D.rbegin() + m + 1, [k](size_t j) { return j <= k; }) - D.rbegin();
   } else {
     s.push_back(*S1.begin());
     for (auto &c : S1) {
+      auto curr = first;
       for (int j = 1; j <= m; ++j) {
-        size_t a1 = D[i - 1][j] + 1, a2 = D[i][j - 1] + 1,
-            a3 = D[i - 1][j - 1] + ((c == *std::next(first, j - 1)) ? 0 : 1);
-        D[i][j]   = std::min({a1, a2, a3});
+        D[i * (m + 1) + j] = std::min({D[(i - 1) * (m + 1) + j] + 1, D[i * (m + 1) + j - 1] + 1,
+                                       D[(i - 1) * (m + 1) + j - 1] + ((c == *(curr++)) ? 0 : 1)});
       }
       // TODO: For maximum performance it is crucial how the value of a q-gram is computed during searching.
       s.back() = c;
-      preprocess_helper_difference(first, last, m, k, q, i + 1, s, S1, M, Ds, D);
+      preprocess_helper_levenshtein(first, last, m, k, q, i + 1, s, S1, M, Ds, D);
     }
     s.pop_back();
   }
 }
+
 /**
- *
+ * Preprocessing of input for Hamming distance
  * @tparam ForwardIt1
  * @tparam SearchedType
  * @param first
  * @param last
- * @param k
- * @param q
- * @param S1
- * @param M
- * @param Ds
+ * @param k number of allowed mistakes
+ * @param q q-gram length
+ * @param S1 alphabet
+ * @param M array of differences between qgram and needle, where deletion in the beginning are free
+ * @param Ds array of the lengthes of jumps for qgrams
  */
 template <class ForwardIt1, class SearchedType>
-void preprocess_mismatch(ForwardIt1 first,
-                         ForwardIt1 last,
-                         size_t k,
-                         size_t q,
-                         std::unordered_set<SearchedType> &S1,
-                         std::map<std::vector<SearchedType>, size_t> &M,
-                         std::map<std::vector<SearchedType>, size_t> &Ds) {
+void preprocess_hamming(ForwardIt1 first,
+                        ForwardIt1 last,
+                        size_t k,
+                        size_t q,
+                        std::unordered_set<SearchedType> &S1,
+                        std::map<std::vector<SearchedType>, size_t> &M,
+                        std::map<std::vector<SearchedType>, size_t> &Ds) {
   size_t m = std::distance(first, last);
-  std::vector<std::vector<size_t>> D(q + 1, std::vector<size_t>(m + 1, 0));
+  std::vector<std::vector<size_t>> D((q + 1) * (m + 1), 0);
   auto s = std::vector<SearchedType>();
   s.reserve(q);
-  preprocess_helper_mismatch(first, last, m, k, q, 1, s, S1, M, Ds, D);
+  preprocess_helper_hamming(first, last, m, k, q, 1, s, S1, M, Ds, D);
 }
 
 /**
- *
+ * Preprocessing of input for Levenshtein distance
  * @tparam ForwardIt1
  * @tparam SearchedType
  * @param first
  * @param last
- * @param k
- * @param q
- * @param S1
- * @param M
- * @param Ds
+ * @param k number of allowed mistakes
+ * @param q q-gram length
+ * @param S1 alphabet
+ * @param M array of differences between qgram and needle, where deletion in the beginning are free
+ * @param Ds array of the lengthes of jumps for qgrams
  */
 template <class ForwardIt1, class SearchedType>
-void preprocess_difference(ForwardIt1 first,
-                           ForwardIt1 last,
-                           size_t k,
-                           size_t q,
-                           std::unordered_set<SearchedType> &S1,
-                           std::map<std::vector<SearchedType>, size_t> &M,
-                           std::map<std::vector<SearchedType>, size_t> &Ds) {
+void preprocess_levenshtein(ForwardIt1 first,
+                            ForwardIt1 last,
+                            size_t k,
+                            size_t q,
+                            std::unordered_set<SearchedType> &S1,
+                            std::map<std::vector<SearchedType>, size_t> &M,
+                            std::map<std::vector<SearchedType>, size_t> &Ds) {
   size_t m = std::distance(first, last);
-  std::vector<std::vector<size_t>> D(q + 1, std::vector<size_t>(m + 1, 0));
+  std::vector<size_t> D((q + 1) * (m + 1), 0);
 
   auto s = std::vector<SearchedType>();
   s.reserve(q);
-  preprocess_helper_difference(first, last, m, k, q, 1, s, S1, M, Ds, D);
+  preprocess_helper_levenshtein(first, last, m, k, q, 1, s, S1, M, Ds, D);
 }
 
 /**
- *
+ * Validate that two strings are withing k errors from each other for Hamming distance
  * @tparam ForwardIt1
  * @tparam ForwardIt2
  * @param first
@@ -172,7 +172,7 @@ void preprocess_difference(ForwardIt1 first,
  * @return
  */
 template <class ForwardIt1, class ForwardIt2>
-bool validate_mismatch(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t m) {  // TODO bidir specialization
+bool validate_hamming(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t m) {  // TODO bidir specialization
   auto c = 0;
   for (int i = m - 1; i >= 0; --i) {
     if (*std::next(first, i) != *std::next(s_first, i)) {  // TODO
@@ -180,11 +180,11 @@ bool validate_mismatch(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t m)
       if (c > k) return false;
     }
   }
-  return true;
+  return true;  // TODO: return position
 }
 
 /**
- *
+ * Validate that two strings are withing k errors from each other for Levenshtein distance
  * @tparam ForwardIt1
  * @tparam ForwardIt2
  * @param first
@@ -194,14 +194,16 @@ bool validate_mismatch(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t m)
  * @return
  */
 template <class ForwardIt1, class ForwardIt2>
-bool validate_difference(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t m) {  // TODO bidir specialization
+bool validate_levenshtein(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t m) {  // TODO bidir specialization
   std::vector<size_t> line1(m + 1, 0), line2(m + 1, 0);
-  std::vector<size_t> *curr=&line1, *prev=&line2;
+  std::vector<size_t> *curr = &line1, *prev = &line2;
   for (int i = 1; i < m + k + 1; ++i) {
+    auto f = *(first++);
+    auto s = s_first;
     for (int j = 1; j < m + 1; ++j) {
-      size_t a1 = (*prev)[j] + 1, a2 = (*curr)[j - 1] + 1,
-          a3 = (*prev)[j - 1] + ((*std::next(first, i - 1) == *std::next(s_first, j - 1)) ? 0 : 1);
-      (*curr)[j]   = std::min({a1, a2, a3});
+      size_t a1 = (*prev)[j] + 1, a2 = (*curr)[j - 1] + 1, a3 = (*prev)[j - 1] + ((f == *(s++)) ? 0 : 1);
+      (*curr)[j] = std::min({a1, a2, a3});
+      if (i == j + k && (*curr)[j] > k) return false;
     }
     std::swap(curr, prev);
   }
@@ -209,7 +211,48 @@ bool validate_difference(ForwardIt1 first, ForwardIt2 s_first, size_t k, size_t 
 }
 
 /**
- *
+ * Validate that two strings are withing k errors from each other for Damerau-Levenshtein distance
+ * @tparam ForwardIt1
+ * @tparam ForwardIt2
+ * @param first
+ * @param s_first
+ * @param k
+ * @param m
+ * @return
+ */
+template <class ForwardIt1, class ForwardIt2>  // TODO weights
+bool validate_damerau_levenshtein(ForwardIt1 first,
+                                  ForwardIt2 s_first,
+                                  size_t k,
+                                  size_t m) {  // TODO bidir specialization
+  std::vector<size_t> line1(m + 1, 0), line2(m + 1, 0), line3(m + 1, 0);
+  std::vector<size_t> *curr = &line1, *prev = &line2, *pprev = &line3;
+  for (int i = 1; i < m + k + 1; ++i) {
+    auto f = *(first++);
+    auto s = s_first;
+    for (int j = 1; j < m + 1; ++j) {
+      size_t a1 = (*prev)[j] + 1, a2 = (*curr)[j - 1] + 1, a3 = (*prev)[j - 1] + ((f == *(s++)) ? 0 : 1),
+          a4 = (i > 1 &&j > 1 &&f = std::prev(s, 2) && std::prev(first, 1) = std::prev(s, 1))
+               ? *(pprev)[j - 2] + 1
+               : a3;  // TODO: forward iter; improve
+      (*curr)[j] = std::min({a1, a2, a3, a4});
+      if (i == j + k && (*curr)[j] > k) return false;
+    }
+
+    std::swap(curr, pprev);
+    std::swap(prev, pprev);
+  }
+  return (*prev)[m] <= k;  // TODO
+}
+
+/**
+ * See
+ * Salmela, Leena, and Jorma Tarhio.
+ * "Approximate string matching with reduced alphabet."
+ * and
+ * Salmela, Leena, Jorma Tarhio, and Petri Kalsi.
+ * "Approximate Boyer-Moore string matching for small alphabets."
+ * for more information on algorithm
  * @tparam ForwardIt1
  * @tparam ForwardIt2
  * @param first The start of the data to search in
@@ -227,12 +270,12 @@ ForwardIt1 fuzzy_search(
   using SearchedType  = typename std::iterator_traits<ForwardIt2>::value_type;
   using SearchingType = typename std::iterator_traits<ForwardIt1>::value_type;
   auto preprocess =
-      mismatch ? preprocess_mismatch<ForwardIt2, SearchedType> : preprocess_difference<ForwardIt2, SearchedType>;
-  auto validate = mismatch ? validate_mismatch<ForwardIt1, ForwardIt2> : validate_difference<ForwardIt1, ForwardIt2>;
+      mismatch ? preprocess_hamming<ForwardIt2, SearchedType> : preprocess_levenshtein<ForwardIt2, SearchedType>;
+  auto validate = mismatch ? validate_hamming<ForwardIt1, ForwardIt2> : validate_levenshtein<ForwardIt1, ForwardIt2>;
 
   // TODO optimal parameters and corner cases
   size_t reduced_alphabet_size = 16;  // size of reduced alphabet
-  size_t q                     = 5;   // size of qgrams
+  size_t q = 6;   // size of qgrams
   // should depend on searched data size
 
   size_t start_search_position = mismatch ? m - q : m - k - q;
